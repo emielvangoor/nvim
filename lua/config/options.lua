216852b174cf2172d85551d6410196172e9abbf1
Emiel <emiel@tesseractpro.io>
2024-06-27 09:44:06 +0200

    Added my initial config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- enable for topbar with filename
+-- vim.opt.winbar = "%=%m %f"
+vim.opt.winbar = ""
+
+vim.opt.clipboard = "unnamedplus"
