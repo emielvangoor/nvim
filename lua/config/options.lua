97ff06d9b8aeb8eb2a89d212935d09ec7b918933
Emiel van Goor <emielvangoor@me.com>
2024-06-27 10:48:59 +0200

    Use tab instead of spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,8 @@
 vim.opt.winbar = ""
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
