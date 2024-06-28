1ee4e1b0e9a0dc9d51017667aabba6bc86fa80b4
Emiel <emiel@tesseractpro.io>
2024-06-28 10:00:16 +0200

    moved themes stuff to its own directory

---
+++ lua/plugins/themes/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  -- { "datsfilipe/vesper.nvim" },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "mocha",
+  --       background = { -- :h background
+  --         light = "latte",
+  --         dark = "mocha",
+  --       },
+  --     })
+  --   end,
+  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
