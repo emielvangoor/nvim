85f8ad3db978f3d140ad1d2c9ebc6b9ccb6fff9d
Emiel van Goor <emielvangoor@me.com>
2024-06-28 10:46:57 +0200

    Added keymaps for turbo arrows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<Leader>fs", ":w<CR>", { silent = true, desc = "Save file" })
+
+-- Map down arrow to }
+vim.api.nvim_set_keymap("n", "<Down>", "}", { noremap = true })
+
+-- Map up arrow to {
+vim.api.nvim_set_keymap("n", "<Up>", "{", { noremap = true })
